3f689db40c5b672881f8a0b72ef8d4db20b3493c
c-h-a-r-l-i-e <c-h-a-r-l-i-e@users.noreply.github.com>
2021-06-24 15:03:31 +0100

    Run evaluation and get results

--- benchmark/run_eval.py
+++ benchmark/run_eval.py
@@ -2,15 +2,11 @@ import os
 import sys
 import evaluate
 import numpy as np
+import json
+import pickle
 
-def blockPrint():
-    sys.stdout = open(os.devnull, 'w')
 
-def enablePrint():
-    sys.stdout = sys.__stdout__
-
-# TODO: figure out how to display these results!
-
+results = {}
 for algo in ["ppo"]:
     for action in ["discrete", "continuous"]:
         for safety in ["safe", "nosafe"]:
@@ -23,9 +19,7 @@ for algo in ["ppo"]:
             checkpoint_folders = os.scandir(base_dir)
             for checkpoint_fold in checkpoint_folders:
                 checkpoint = checkpoint_fold.path + "/checkpoint_100/checkpoint-100"
-                blockPrint()
-                agent_mets = evaluate.main("scenarios/double_merge/cross/", [config_file], checkpoint, ".log/results", num_episodes=1, headless=True)
-                enablePrint()
+                agent_mets = evaluate.main("scenarios/double_merge/cross/", [config_file], checkpoint, ".log/results", num_episodes=10, headless=True)
 
                 complete_rate = agent_mets['']['Completion Rate']
                 collision_rate = agent_mets['']['Average Collision Rate']
@@ -40,12 +34,17 @@ for algo in ["ppo"]:
             coll_mean = np.mean(collision_rates)
             coll_std = np.std(collision_rates)
 
-            print(f"completion rate = {comp_mean} +- {1.95 * comp_std / np.sqrt(10)}")
-            print(f"collision rate = {coll_mean} +- {1.95 * coll_std / np.sqrt(10)}")
+            comp_str = f"completion rate = {comp_mean} +- {1.95 * comp_std / np.sqrt(10)}"
+            coll_str = f"collision rate = {coll_mean} +- {1.95 * coll_std / np.sqrt(10)}"
+            results[f"{algo} - {action} - {safety}"] = comp_str, coll_str
+
+            print(comp_str)
+            print(coll_str)
 
 
+print(json.dumps(results, sort_keys=True, indent=4))
 
-
-
+with open("eval.pickle", "wb") as f:
+    pickle.dump(results, f)
 
 
